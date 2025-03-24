746e0ccf90294d681de1bce163d2e2957a36a88a
Angel Aldaz <angel.d.aldaz@gmail.com>
2025-03-24 01:21:26 -0600

    refactor(sections/footer): ♻️ modularize layout with footer
    Added "footer.php" to further modularize the existing layout   for the footer section.

---
+++ SubDom2025/src/sections/footer.php
@@ -0,0 +1,66 @@
+<footer id="footer">
+  <div class="footer-top">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-3 col-md-6">
+          <div class="footer-info">
+            <h3 style="color: #d16300">ICAIMH 2025</h3>
+
+            <div class="social-links mt-4">
+              <a
+                href="https://twitter.com/ICASST22"
+                class="twitter"
+                target="_blank"><i class="bx bxl-twitter"></i></a>
+              <a
+                href="https://www.facebook.com/profile.php?id=100088870880682"
+                class="facebook"
+                target="_blank"><i class="bx bxl-facebook"></i></a>
+              <a
+                href="https://www.linkedin.com/company/international-conference-on-aerospace-science-and-technology-icasst/"
+                class="linkedin"
+                target="_blank"><i class="bx bxl-linkedin"></i></a>
+              <a
+                href="https://youtube.com/@user-qe8jf8ti5r"
+                class="linkedin"
+                target="_blank"><i class="bx bxl-youtube"></i></a>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-lg-2 col-md-6 footer-links">
+          <h4>Main Site</h4>
+          <ul>
+            <li>
+              <i class="bx bx-chevron-right"></i>
+              <a href="https://icaimh.org">https://icaimh.org</a>
+            </li>
+          </ul>
+        </div>
+
+        <div class="col-lg-3 col-md-6 footer-links">
+          <h4>Previous Conferences</h4>
+          <ul>
+            <li>
+              <i class="bx bx-chevron-right"></i>
+              <a href="https://2024.icaimh.org">https://2024.icaimh.org</a>
+            </li>
+            <li>
+              <i class="bx bx-chevron-right"></i>
+              <a href="https://2023.icaimh.org">https://2023.icaimh.org</a>
+            </li>
+          </ul>
+        </div>
+
+        <div class="col-lg-4 col-md-6 footer-newsletter">
+          <h4>
+            International Conference on Artificial Intelligence for Mental
+            Health
+          </h4>
+          <div>
+            &copy; Copyright <strong><span>ICAIMH</span></strong>. All Rights Reserved.
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</footer>
