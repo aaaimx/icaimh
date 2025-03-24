0b3caa2259d8a6defc1c9be1af53af1bc99006a5
Angel Aldaz <angel.d.aldaz@gmail.com>
2025-03-24 01:06:05 -0600

    refactor(sections/prelProgSection): ♻️ modularize layout with prelProgSection
    Added "prelProgSection.php" to further modularize the existing layout   for the preliminary program of the conference.

---
+++ SubDom2025/src/sections/prelProgSection.php
@@ -0,0 +1,157 @@
+<!-- Start Schedule Section -->
+<section id="schedule" class="section-bg">
+  <div class="container" data-aos="fade-up">
+    <div class="section-title">
+      <h1>Preliminary program</h1>
+    </div>
+
+    <ul
+      class="nav nav-tabs"
+      role="tablist"
+      data-aos="fade-up"
+      data-aos-delay="100">
+      <li class="nav-item">
+        <a
+          class="nav-link active"
+          href="#day-1"
+          role="tab"
+          data-bs-toggle="tab">2-JULY</a><br />
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#day-2" role="tab" data-bs-toggle="tab">3-JULY</a><br />
+      </li>
+
+      <!--
+          <li class="nav-item">
+            <a class="nav-link" href="#day-4" role="tab" data-bs-toggle="tab">1-DEC</a><br>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#day-5" role="tab" data-bs-toggle="tab">2-DEC</a><br>
+          </li>
+          -->
+    </ul>
+
+    <h3 class="sub-heading"></h3>
+
+    <div
+      class="tab-content row justify-content-center"
+      data-aos="fade-up"
+      data-aos-delay="200">
+      <div
+        role="tabpanel"
+        class="col-lg-7 tab-pane fade show active"
+        id="day-1">
+        <div class="container d-flex flex-column">
+          <!--  
+            <div class="row schedule-item d-flex justify-content-between">
+              <div class="col-md-6" style="text-align: justify">
+                El rol de la inteligencia artificial en la salud mental
+              </div>
+              <div class="col-md-6 fw-bold" style="text-align: justify">
+                The role of artificial intelligence in mental health
+              </div>
+            </div>
+            <div class="row schedule-item d-flex justify-content-between">
+              <div class="col-md-6" style="text-align: justify">
+                Aplicaciones de ciencia de datos para la salud mental
+              </div>
+              <div class="col-md-6 fw-bold" style="text-align: justify">
+                Data Science Applications for Mental Health
+              </div>
+            </div>
+            <div class="row schedule-item d-flex justify-content-between">
+              <div class="col-md-6" style="text-align: justify">
+                Análisis Exploratorio de Datos para Identificar la Relación entre Depresión-Ansiedad y Factores Espirituales-Religiosos
+              </div>
+              <div class="col-md-6 fw-bold" style="text-align: justify">
+                Exploratory Data Analysis to Identify the Relationship Between Depression-Anxiety and Spiritual-Religious Factors among Undergraduate Students
+              </div>
+            </div>
+            <div class="row schedule-item d-flex justify-content-between">
+              <div class="col-md-6" style="text-align: justify">
+                IREX: Refinamiento de cuestionarios psicológicos utilizando métodos de inteligencia artificial explicable
+              </div>
+              <div class="col-md-6 fw-bold" style="text-align: justify">
+                IREX: Refinement of psychological questionnaires using explainable artificial intelligence methods
+              </div>
+            </div>
+            <div class="row schedule-item d-flex justify-content-between">
+              <div class="col-md-6" style="text-align: justify">
+                Cuidado de la salud mental: concepto, prevención y estigma en las sociedades actuales
+              </div>
+              <div class="col-md-6 fw-bold" style="text-align: justify">
+                Mental health care: concept, prevention and stigma in today's societies
+              </div>
+            </div>
+          -->
+          TBA
+        </div>
+      </div>
+      <div role="tabpanel" class="col-lg-7 tab-pane fade" id="day-2">
+        <div class="container d-flex flex-column">
+          <!--
+              <div class="row schedule-item d-flex justify-content-between">
+                <div class="col-md-6" style="text-align: justify">
+                  Panorama actual del desarrollo de videojuegos
+                </div>
+                <div class="col-md-6 fw-bold" style="text-align: justify">
+                  Current panorama of video game development
+                </div>
+              </div>
+              <div class="row schedule-item d-flex justify-content-between">
+                <div class="col-md-6" style="text-align: justify">
+                  Uso de técnicas de learning analytics para la validación, mejora y aplicación de juegos serios en la clase aplicado al ciberbullyng
+                </div>
+                <div class="col-md-6 fw-bold" style="text-align: justify">
+                  Use of learning analytics techniques for the validation, improvement and application of serious games in the classroom applied to cyberbullying
+                </div>
+              </div>
+              <div class="row schedule-item d-flex justify-content-between">
+                <div class="col-md-6" style="text-align: justify">
+                  Implementación de técnicas para el tratamiento de síntomas de ansiedad para el desarrollo de un videojuego con biorretroalimentación
+                </div>
+                <div class="col-md-6 fw-bold" style="text-align: justify">
+                  Implementation of techniques for the treatment of anxiety symptoms for the development of a video game with biofeedback
+                </div>
+              </div>
+              <div class="row schedule-item d-flex justify-content-between">
+                <div class="col-md-6" style="text-align: justify">
+                  Competición Internacional de Comportamientos Inteligentes de MsPacMan vs Ghosts - 2023
+                </div>
+                <div class="col-md-6 fw-bold" style="text-align: justify">
+                  International Competition of Intelligent Behaviors of MsPacman vs Ghosts
+                </div>
+              </div>
+              <div class="row schedule-item d-flex justify-content-between">
+                <div class="col-md-6" style="text-align: justify">
+                  El uso de videojuegos para la evaluación de la ansiedad
+                </div>
+                <div class="col-md-6 fw-bold" style="text-align: justify">
+                  The use of videogames for the assessment of anxiety
+                </div>
+              </div>
+              <div class="row schedule-item d-flex justify-content-between">
+                <div class="col-md-6" style="text-align: justify">
+                  Análisis del apoyo en la identificación de distorsiones cognitivas en adultos jóvenes mediante un videojuego ad hoc
+                </div>
+                <div class="col-md-6 fw-bold" style="text-align: justify">
+                  Analysis of support in the identification of cognitive distortions in young adults through an ad hoc video game
+                </div>
+              </div>
+              <div class="row schedule-item d-flex justify-content-between">
+                <div class="col-md-6" style="text-align: justify">
+                  Desarrollo de videojuegos con Unity
+                </div>
+                <div class="col-md-6 fw-bold" style="text-align: justify">
+                  Game development with Unity
+                </div>
+              </div>
+            -->
+          TBA
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+<!-- End Schedule Section -->
