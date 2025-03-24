33c326e8475f154149f6e8e38e87cca42226b137
Angel Aldaz <angel.d.aldaz@gmail.com>
2025-03-24 00:55:14 -0600

    feat(sections/navBar): ✨ add initial navBar section
    Created the "sections" folder and added the first section "navBar.php".

---
+++ SubDom2025/src/sections/navBar.php
@@ -0,0 +1,47 @@
+<nav class="navbar navbar-expand-lg navbar-light py-2">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="/">
+      <img src="/public/img/logo/logo_icaimh.png" alt="icaimh" width="60" />
+    </a>
+    <button
+      class="navbar-toggler"
+      type="button"
+      data-bs-toggle="collapse"
+      data-bs-target="#navbarSupportedContent"
+      aria-controls="navbarSupportedContent"
+      aria-expanded="false"
+      aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div
+      class="justify-content-end collapse navbar-collapse"
+      id="navbarSupportedContent">
+      <ul class="navbar-nav ml-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="/">Home</a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link" href="#venue">Venue</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#cfp">Call for papers</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#organizers">Organizers</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#partners">Partners</a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link" href="#contact">Contact</a>
+        </li>
+
+        <li class="nav-item button-join">
+          <a class="" href="/join">Join Now</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
