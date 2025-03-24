e3bc116fefab09b9888cafe088bc2a95d9b92c76
Angel Aldaz <angel.d.aldaz@gmail.com>
2025-03-24 01:12:50 -0600

    refactor(sections, objects : programChairs): ♻️ modularize programChairsSection with committeeCardComponent
    Modularized "programChairsSection.php" using "committeeCardComponent.php".   Added "programChairs.php" to store chair information,   allowing iteration within the section.

---
+++ SubDom2025/src/sections/programChairsSection.php
@@ -0,0 +1,31 @@
+<!-- ======= Start Pogram Chairs Section ======= -->
+<section
+  id="organizers"
+  class="p-5 text-center"
+  style="max-width: 1300px; margin: auto">
+  <h1 class="d-inline-block">PROGRAM CHAIRS</h1>
+  <div class="d-flex justify-content-center py-3">
+    <div class="testimonial3 py-5">
+      <!-- 
+        <div class="row justify-content-center">
+          <div class="col-md-8 text-center">
+            <h4 class="mb-3">Check what our Customers are Saying</h4>
+            <h6 class="subtitle font-weight-normal">You can relay on our amazing features list and also our customer services will be great experience for you without doubt</h6>
+          </div>
+        </div> 
+      -->
+
+      <div class="container">
+        <div class="testi3 row justify-content-center">
+          <?php
+          include("src/objects/programChairs.php");
+          foreach ($programChairs as $person) {
+            renderCard($person["first_name"], $person["last_name"], $person["title"], $person["university"], $person["br"]);
+          }
+          ?>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<!-- ======= End Pogram Chairs Section ======= -->
