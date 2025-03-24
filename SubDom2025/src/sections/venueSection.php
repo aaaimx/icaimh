ec1fc13d2266039d17aee4f6f6924d6f6295dc8a
Angel Aldaz <angel.d.aldaz@gmail.com>
2025-03-24 00:59:28 -0600

    refactor(sections/venue): ♻️ modularize layout with venueSection
    Added "venueSection.php" to further modularize the existing layout.

---
+++ SubDom2025/src/sections/venueSection.php
@@ -0,0 +1,39 @@
+<!--Galeria 2022-->
+
+<section id="gallery">
+  <div class="container">
+    <div id="venue" class="section-title">
+      <h1>Venue</h1>
+    </div>
+
+    <p style="text-align: justify">
+      We are pleased to announce that ICAIMH 2025 will be held in Mérida,
+      Yucatán, México. The conference will take place at the
+      <a href="https://merida.anahuac.mx/" target="_blank"><b><i>Anáhuac Mayab University</i></b></a>. The Anáhuac Mayab University is a private institution of higher
+      education located in Mérida, Yucatán, México. The university is part
+      of the
+      <a href="https://www.anahuac.mx/" target="_blank"><b><i>Universidad Anáhuac</i></b></a>
+      network, which is one of the most prestigious private educational
+      institutions in México. The Anáhuac Mayab University is located in
+      the northern part of the city of Mérida, which is the capital of the
+      state of Yucatán. Mérida is a city with a rich history and culture,
+      and it is known for its colonial architecture, its gastronomy, and
+      its friendly people. The city is also known for its proximity to
+      important archaeological sites such as Chichen Itza, Uxmal, and
+      Dzibilchaltún. Mérida is also known for its proximity to beautiful
+      beaches such as Progreso, Celestún, and Sisal. We are looking
+      forward to welcoming you to Mérida for ICAIMH 2025!
+    </p>
+
+    <div id="galeria" class="section-title"></div>
+    <video width="100%" height="100%" loop autoplay muted>
+      <source
+        src="https://f.hubspotusercontent40.net/hubfs/3416190/Banner_Web_4_Video.mp4"
+        type="video/mp4" />
+      <source src="" type="video/ogg" />
+      <source src="" type="video/webm" />
+    </video>
+  </div>
+</section>
+
+<!--End Galeria 2022-->
