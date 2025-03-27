8384964e161f521d829eb01a48d0aa890e4a6356
Angel Aldaz <angel.d.aldaz@gmail.com>
2025-03-27 16:31:44 -0600

    test(join): 🐛 Removed the Join button

--- SubDom2025/src/sections/navBar.php
+++ SubDom2025/src/sections/navBar.php
@@ -38,9 +38,9 @@
           <a class="nav-link" href="#contact">Contact</a>
         </li>
 
-        <li class="nav-item button-join">
+        <!-- <li class="nav-item button-join">
           <a class="" href="/join">Join Now</a>
-        </li>
+        </li> -->
       </ul>
     </div>
   </div>
