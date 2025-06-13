aecbad89f9003abeaf536ed63779731212040fdc
Angel Aldaz <angel.d.aldaz@gmail.com>
2025-06-13 04:24:07 -0600

    feat(schedule): add initial schedule CSS and PHP modules for day one and two
    - Added new CSS file for schedule styling with color variables and session styles
    - Created PHP arrays for day one and day two schedule modules with sessions, times, speakers, and colors
    - Updated preliminary program section to dynamically render schedule from PHP modules
    - Changed section title from "Preliminary program" to "SCHEDULE"

---
+++ SubDom2025/src/objects/modules_day_one.php
@@ -0,0 +1,34 @@
+<?php
+$modules_day_one = [
+  [
+    "time_start" => "09:00",
+    "time_end" => "09:30",
+    "session" => "Recepción",
+    "color" => "logistics"
+  ],
+  [
+    "time_start" => "09:30",
+    "time_end" => "10:00",
+    "session" => "Bienvenida",
+    "color" => "logistics"
+  ],
+  [
+    "time_start" => "10:00",
+    "time_end" => "11:00",
+    "session" => "X",
+    "color" => "keynote"
+  ],
+  [
+    "time_start" => "11:00",
+    "time_end" => "11:30",
+    "session" => "Coffee break",
+    "color" => "coffee-break"
+  ],
+  [
+    "time_start" => "11:30",
+    "time_end" => "12:30",
+    "session" => "La Inteligencia Artificial como Herramienta para Abordar los Desafíos en Salud Mental: Un Debate Multidisciplinario",
+    "speaker" => "Romhmy Patricia Hidalgo Carrillo",
+    "color" => "talk"
+  ],
+];
--- SubDom2025/src/sections/prelProgSection.php
+++ SubDom2025/src/sections/prelProgSection.php
@@ -1,8 +1,16 @@
 <!-- Start Schedule Section -->
+<?php
+include("src/objects/modules_day_one.php");
+include("src/objects/modules_day_two.php");
+?>
+
+<head>
+  <link rel="stylesheet" href="src/css/schedule.css">
+</head>
 <section id="schedule" class="section-bg">
   <div class="container" data-aos="fade-up">
     <div class="section-title">
-      <h1>Preliminary program</h1>
+      <h1>SCHEDULE</h1>
     </div>
 
     <ul
@@ -20,15 +28,6 @@
       <li class="nav-item">
         <a class="nav-link" href="#day-2" role="tab" data-bs-toggle="tab">3-JULY</a><br />
       </li>
-
-      <!--
-          <li class="nav-item">
-            <a class="nav-link" href="#day-4" role="tab" data-bs-toggle="tab">1-DEC</a><br>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#day-5" role="tab" data-bs-toggle="tab">2-DEC</a><br>
-          </li>
-          -->
     </ul>
 
     <h3 class="sub-heading"></h3>
@@ -41,113 +40,31 @@
         role="tabpanel"
         class="col-lg-7 tab-pane fade show active"
         id="day-1">
-        <div class="container d-flex flex-column">
-          <!--  
-            <div class="row schedule-item d-flex justify-content-between">
-              <div class="col-md-6" style="text-align: justify">
-                El rol de la inteligencia artificial en la salud mental
-              </div>
-              <div class="col-md-6 fw-bold" style="text-align: justify">
-                The role of artificial intelligence in mental health
-              </div>
+        <div class="schedule">
+          <?php foreach ($modules_day_one as $module) { ?>
+            <div class="time-slot"><?php echo htmlspecialchars($module['time_start']); ?></div>
+            <div class="session <?php echo htmlspecialchars($module['color']); ?>">
+              <h3><?php echo htmlspecialchars($module['session']); ?></h3>
+              <?php if (isset($module['speaker'])) { ?>
+                <span><?php echo htmlspecialchars($module['time_start']); ?> - <?php echo htmlspecialchars($module['time_end']); ?></span>
+                <span><?php echo htmlspecialchars($module['speaker']); ?></span>
+              <?php } ?>
             </div>
-            <div class="row schedule-item d-flex justify-content-between">
-              <div class="col-md-6" style="text-align: justify">
-                Aplicaciones de ciencia de datos para la salud mental
-              </div>
-              <div class="col-md-6 fw-bold" style="text-align: justify">
-                Data Science Applications for Mental Health
-              </div>
-            </div>
-            <div class="row schedule-item d-flex justify-content-between">
-              <div class="col-md-6" style="text-align: justify">
-                Análisis Exploratorio de Datos para Identificar la Relación entre Depresión-Ansiedad y Factores Espirituales-Religiosos
-              </div>
-              <div class="col-md-6 fw-bold" style="text-align: justify">
-                Exploratory Data Analysis to Identify the Relationship Between Depression-Anxiety and Spiritual-Religious Factors among Undergraduate Students
-              </div>
-            </div>
-            <div class="row schedule-item d-flex justify-content-between">
-              <div class="col-md-6" style="text-align: justify">
-                IREX: Refinamiento de cuestionarios psicológicos utilizando métodos de inteligencia artificial explicable
-              </div>
-              <div class="col-md-6 fw-bold" style="text-align: justify">
-                IREX: Refinement of psychological questionnaires using explainable artificial intelligence methods
-              </div>
-            </div>
-            <div class="row schedule-item d-flex justify-content-between">
-              <div class="col-md-6" style="text-align: justify">
-                Cuidado de la salud mental: concepto, prevención y estigma en las sociedades actuales
-              </div>
-              <div class="col-md-6 fw-bold" style="text-align: justify">
-                Mental health care: concept, prevention and stigma in today's societies
-              </div>
-            </div>
-          -->
-          TBA
+          <?php } ?>
         </div>
       </div>
       <div role="tabpanel" class="col-lg-7 tab-pane fade" id="day-2">
-        <div class="container d-flex flex-column">
-          <!--
-              <div class="row schedule-item d-flex justify-content-between">
-                <div class="col-md-6" style="text-align: justify">
-                  Panorama actual del desarrollo de videojuegos
-                </div>
-                <div class="col-md-6 fw-bold" style="text-align: justify">
-                  Current panorama of video game development
-                </div>
-              </div>
-              <div class="row schedule-item d-flex justify-content-between">
-                <div class="col-md-6" style="text-align: justify">
-                  Uso de técnicas de learning analytics para la validación, mejora y aplicación de juegos serios en la clase aplicado al ciberbullyng
-                </div>
-                <div class="col-md-6 fw-bold" style="text-align: justify">
-                  Use of learning analytics techniques for the validation, improvement and application of serious games in the classroom applied to cyberbullying
-                </div>
-              </div>
-              <div class="row schedule-item d-flex justify-content-between">
-                <div class="col-md-6" style="text-align: justify">
-                  Implementación de técnicas para el tratamiento de síntomas de ansiedad para el desarrollo de un videojuego con biorretroalimentación
-                </div>
-                <div class="col-md-6 fw-bold" style="text-align: justify">
-                  Implementation of techniques for the treatment of anxiety symptoms for the development of a video game with biofeedback
-                </div>
-              </div>
-              <div class="row schedule-item d-flex justify-content-between">
-                <div class="col-md-6" style="text-align: justify">
-                  Competición Internacional de Comportamientos Inteligentes de MsPacMan vs Ghosts - 2023
-                </div>
-                <div class="col-md-6 fw-bold" style="text-align: justify">
-                  International Competition of Intelligent Behaviors of MsPacman vs Ghosts
-                </div>
-              </div>
-              <div class="row schedule-item d-flex justify-content-between">
-                <div class="col-md-6" style="text-align: justify">
-                  El uso de videojuegos para la evaluación de la ansiedad
-                </div>
-                <div class="col-md-6 fw-bold" style="text-align: justify">
-                  The use of videogames for the assessment of anxiety
-                </div>
-              </div>
-              <div class="row schedule-item d-flex justify-content-between">
-                <div class="col-md-6" style="text-align: justify">
-                  Análisis del apoyo en la identificación de distorsiones cognitivas en adultos jóvenes mediante un videojuego ad hoc
-                </div>
-                <div class="col-md-6 fw-bold" style="text-align: justify">
-                  Analysis of support in the identification of cognitive distortions in young adults through an ad hoc video game
-                </div>
-              </div>
-              <div class="row schedule-item d-flex justify-content-between">
-                <div class="col-md-6" style="text-align: justify">
-                  Desarrollo de videojuegos con Unity
-                </div>
-                <div class="col-md-6 fw-bold" style="text-align: justify">
-                  Game development with Unity
-                </div>
-              </div>
-            -->
-          TBA
+        <div class="schedule">
+          <?php foreach ($modules_day_two as $module) { ?>
+            <div class="time-slot"><?php echo htmlspecialchars($module['time_start']); ?></div>
+            <div class="session <?php echo htmlspecialchars($module['color']); ?>">
+              <h3><?php echo htmlspecialchars($module['session']); ?></h3>
+              <?php if (isset($module['speaker'])) { ?>
+                <span><?php echo htmlspecialchars($module['time_start']); ?> - <?php echo htmlspecialchars($module['time_end']); ?></span>
+                <span><?php echo htmlspecialchars($module['speaker']); ?></span>
+              <?php } ?>
+            </div>
+          <?php } ?>
         </div>
       </div>
     </div>
