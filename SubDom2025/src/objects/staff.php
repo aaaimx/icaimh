b1ed5df3ca3cacd3f32e67fdd34a97a1caa58987
Angel Aldaz <angel.d.aldaz@gmail.com>
2025-06-13 04:21:54 -0600

    feat(staff): add staff members to staff object
        - Adds several new staff members to the staff object.
        - Includes first name, last name, title, university, and br attributes.

--- SubDom2025/src/objects/staff.php
+++ SubDom2025/src/objects/staff.php
@@ -6,5 +6,40 @@ $staff = [
     "title" => "Web Developer",
     "university" => "Tecnológico Nacional de México/Instituto Tecnológico de Mérida, Mexico.",
     "br" => null
+  ],
+  [
+    "first_name" => "David Alejandro",
+    "last_name" => "Poot-Osorio",
+    "title" => "Staff",
+    "university" => "Tecnológico Nacional de México/Instituto Tecnológico de Mérida, Mexico.",
+    "br" => null
+  ],
+  [
+    "first_name" => "Juan Rafael",
+    "last_name" => "Almeida-Rivera",
+    "title" => "Staff",
+    "university" => "Tecnológico Nacional de México/Instituto Tecnológico de Mérida, Mexico.",
+    "br" => null
+  ],
+  [
+    "first_name" => "Maria Fernanda",
+    "last_name" => "Aldama-Pech",
+    "title" => "Staff",
+    "university" => "Tecnológico Nacional de México/Instituto Tecnológico de Mérida, Mexico.",
+    "br" => null
+  ],
+  [
+    "first_name" => "Alejandro Daniel",
+    "last_name" => "Espinosa-Chim",
+    "title" => "Staff",
+    "university" => "Tecnológico Nacional de México/Instituto Tecnológico de Mérida, Mexico.",
+    "br" => null
+  ],
+  [
+    "first_name" => "Brayan Michel",
+    "last_name" => "Reyes-Castillo",
+    "title" => "Staff",
+    "university" => "Tecnológico Nacional de México/Instituto Tecnológico de Mérida, Mexico.",
+    "br" => null
   ]
 ];
