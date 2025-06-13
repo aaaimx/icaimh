12da1edd5aed26bf3d62ebac48ef5b06646fb0c5
Angel Aldaz <angel.d.aldaz@gmail.com>
2025-06-13 15:21:51 -0600

    feat(speakers): add talk titles for invited speakers

--- SubDom2025/src/objects/invited_speakers.php
+++ SubDom2025/src/objects/invited_speakers.php
@@ -11,21 +11,21 @@ $invited_speakers = [
   [
     "name" => "Mtra. Mariana Sologuren López",
     "type" => "Invited Talk",
-    "talk_title" => null,
+    "talk_title" => "¿Puede una máquina sostener el alma? Salud mental y los vínculos en la era de la Inteligencia Artificial",
     "info" => "Mariana Sologuren López es Licenciada en Psicología y Maestra en Psicoterapia Individual y Grupal, egresada de la Universidad Modelo. Cuenta con una sólida trayectoria clínica y académica. Ha trabajado en el ámbito de la salud mental tanto en el sector privado como institucional, destacando su labor en clínicas especializadas en adicciones.\nDesde hace más de cinco años, de igual manera, se ha desempeñado en el ámbito educativo, fortaleciendo su enfoque en los procesos de aprendizaje, desarrollo humano y acompañamiento psicológico en contextos formativos.\nActualmente, dirige su consultorio privado, donde ofrece atención psicoterapéutica a población infantil, adolescentes y adultos, desde un enfoque integrativo que se adapta a las necesidades específicas de cada persona.",
     "img" => "../public/img/speakers/mariana-sologuren-lopez.jpg"
   ],
   [
     "name" => "Dr. Eduardo Barbará Morales",
     "type" => "Invited Talk",
-    "talk_title" => null,
+    "talk_title" => "Impacto del COVID-19 en la morfología cerebral: un enfoque cuantitativo",
     "info" => "El Dr. Eduardo Barbará Morales es graduado con la medalla al mérito universitario en el Doctorado en Ingeniería Biomédica por la Universidad Autónoma Metropolitana, unidad Iztapalapa (UAM-I), en la ciudad de México. Cuenta con una maestría en el área de Ingeniería Biomédica por la Universidad Tecnológica de la Habana José A. Echeverría y es Ingeniero en Telecomunicaciones y Electrónica por dicha institución.\nEl Dr. Barbará cuenta con más de 15 años de experiencia docente y de investigación en diferentes universidades de Cuba, México y Colombia en el área de ingeniería Biomédica. Ha sido autor de varias publicaciones en revistas internacionales del área biomédica y ha participado en diversos congresos y eventos nacionales e internacionales.\nActualmente es profesor e investigador de tiempo completo en la División de Ingeniería y Ciencias Exactas de la Universidad Anáhuac Mayab. Es miembro del Sistema Nacional de Investigadoras e Investigadores (SNII) nivel: candidato a investigador nacional, de la sociedad Mexicana de Ingeniería Biomédica (SOMIB), del registro Estatal de Investigadores del estado de Yucatán (REI) y miembro del comité de investigación de la Universidad Anáhuac Mayab.",
     "img" => "../public/img/speakers/eduardo-barbara-morales.jpg"
   ],
   [
     "name" => "Dr. Juan Antonio Recio García",
     "type" => "Invited Closing Talk",
-    "talk_title" => null,
+    "talk_title" => "Cuando las máquinas deciden: Ética y responsabilidad en la era de la Inteligencia Artificial",
     "info" => "Doctor con Mención Europea en Inteligencia Artificial por la Universidad Complutense de Madrid, donde desarrolla su labor docente e investigadora desde hace más de 20 años. Actualmente ocupa el puesto de Profesor Titular y entre 2015 y 2019 dirigió el Departamento de Ingeniería del Software e Inteligencia Artificial. Hoy en día dirige la Cátedra Honorífica de Inteligencia Artificial aplicada al Internet de las Cosas de dicha universidad. Además, es miembro del Instituto Internacional para las Tecnologías Inteligentes (https://iiitechs.org).\nHa colaborado con universidades en Reino Unido, Irlanda y México, y desarrollado numerosos proyectos de I+D con empresas. Actualmente lidera varios proyectos de investigación, tanto a nivel nacional como financiados por la Unión Europea en el ámbito de la Inteligencia Artificial Explicable.\nEl profesor Recio ha publicado un centenar de trabajos de investigación y recibido distintos premios por sus contribuciones científicas para conseguir que las máquinas sean más inteligentes y comprensibles, con aplicaciones tanto en investigación como en la industria.",
     "img" => "../public/img/speakers/juan-antonio-recio.jpg"
   ]
