bf382b42fa1935dd448b8a4f30120029cd248eac
Angel Aldaz <angel.d.aldaz@gmail.com>
2025-06-28 10:59:55 -0600

    feat(staff): add new staff members and reorder existing entries

--- SubDom2025/src/objects/staff.php
+++ SubDom2025/src/objects/staff.php
@@ -1,9 +1,23 @@
 <?php
 $staff = [
   [
+    "first_name" => "Alejandro Daniel",
+    "last_name" => "Espinosa-Chim",
+    "title" => "Staff",
+    "university" => "Tecnológico Nacional de México/Instituto Tecnológico de Mérida, Mexico.",
+    "br" => null
+  ],
+  [
     "first_name" => "Ángel Daniel",
     "last_name" => "Aldaz-Carrillo",
     "title" => "Web Developer",
+    "university" => "Tecnológico Nacional de México/Instituto Tecnológico de Mérida, Mexico.",
+    "br" => null
+  ],
+  [
+    "first_name" => "Brayan Michel",
+    "last_name" => "Reyes-Castillo",
+    "title" => "Staff",
     "university" => "Tecnológico Nacional de México/Instituto Tecnológico de Mérida, Mexico.",
     "br" => null
   ],
@@ -15,8 +29,36 @@ $staff = [
     "br" => null
   ],
   [
+    "first_name" => "Hannah Sofía",
+    "last_name" => "Navarro",
+    "title" => "Staff",
+    "university" => "Anáhuac Mayab, México.",
+    "br" => null
+  ],
+  [
+    "first_name" => "Jenery",
+    "last_name" => "Bautista-Canul",
+    "title" => "Staff",
+    "university" => "Tecnológico Nacional de México/Instituto Tecnológico de Mérida, Mexico.",
+    "br" => null
+  ],
+  [
+    "first_name" => "José Leopoldo",
+    "last_name" => "Cabieses",
+    "title" => "Staff",
+    "university" => "Anáhuac Mayab, México.",
+    "br" => null
+  ],
+  [
     "first_name" => "Juan Rafael",
     "last_name" => "Almeida-Rivera",
+    "title" => "Staff",
+    "university" => "Tecnológico Nacional de México/Instituto Tecnológico de Mérida, Mexico.",
+    "br" => null
+  ],
+  [
+    "first_name" => "Luis",
+    "last_name" => "Cardeña-Ley",
     "title" => "Staff",
     "university" => "Tecnológico Nacional de México/Instituto Tecnológico de Mérida, Mexico.",
     "br" => null
@@ -29,17 +71,17 @@ $staff = [
     "br" => null
   ],
   [
-    "first_name" => "Alejandro Daniel",
-    "last_name" => "Espinosa-Chim",
+    "first_name" => "Mariana",
+    "last_name" => "García-Álvarez",
     "title" => "Staff",
-    "university" => "Tecnológico Nacional de México/Instituto Tecnológico de Mérida, Mexico.",
+    "university" => "Anáhuac Mayab, México.",
     "br" => null
   ],
   [
-    "first_name" => "Brayan Michel",
-    "last_name" => "Reyes-Castillo",
+    "first_name" => "Natalia",
+    "last_name" => "Nakamura-Chocolatl",
     "title" => "Staff",
-    "university" => "Tecnológico Nacional de México/Instituto Tecnológico de Mérida, Mexico.",
+    "university" => "Anáhuac Mayab, México.",
     "br" => null
   ],
   [
@@ -47,13 +89,6 @@ $staff = [
     "last_name" => "Ocampo-Valencia",
     "title" => "Staff",
     "university" => "Universidad de Cundinamarca. Colombia.",
-    "br" => null
-  ],
-  [
-    "first_name" => "Luis",
-    "last_name" => "Cardeña-Ley",
-    "title" => "Staff",
-    "university" => "Tecnológico Nacional de México/Instituto Tecnológico de Mérida, Mexico.",
     "br" => null
   ]
 ];
