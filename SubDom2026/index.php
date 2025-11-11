e06512b025fe5973092a98bf9e21f96784a5c23a
Angel Aldaz <angel.d.aldaz@gmail.com>
2025-11-11 11:53:23 -0600

    chore(branding): actualizar título del sitio para evento 2026
    - Cambiar título de "ICAIMH 2025" a "ICAIMH 2026"
    - Mantener estructura y metadatos existentes
    - Preparar identificación visual para nuevo ciclo del evento

--- SubDom2026/index.php
+++ SubDom2026/index.php
@@ -5,7 +5,7 @@
   <meta charset="utf-8" />
   <meta content="width=device-width, initial-scale=1.0" name="viewport" />
 
-  <title>ICAIMH 2025</title>
+  <title>ICAIMH 2026</title>
   <meta content="" name="description" />
   <meta content="" name="keywords" />
 
