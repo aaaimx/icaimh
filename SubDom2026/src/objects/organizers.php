d3b9cbfec3ac5a570a4d2a2df51077e79332f960
Angel Aldaz <angel.d.aldaz@gmail.com>
2025-11-11 11:59:13 -0600

    chore(committee): limpiar listas de comités y staff para nueva configuración 2026
    - Comentar lista completa de invited_speakers (9 ponentes)
    - Comentar lista de organizers (3 organizadores)
    - Comentar lista de programChairs (3 directores de programa)
    - Comentar lista de programCommittee (28 miembros)
    - Comentar lista de sponsors (26 patrocinadores)
    - Comentar lista de staff (14 miembros del equipo)
    - Mantener estructura de arrays para fácil reactivación
    - Preparar base limpia para nueva configuración de comités 2026

--- SubDom2026/src/objects/organizers.php
+++ SubDom2026/src/objects/organizers.php
@@ -1,18 +1,18 @@
 <?php
 $organizers = [
-  [
-    "name" => "Association for the Advancement of Intelligent Applications and Technologies with Social Impact",
-    "img" => "/public/img/maikron.jpg",
-    "url" => "https://maikron.org",
-  ],
-  [
-    "name" => "Asociación para la Salud Mental de Yucatán",
-    "img" => "/public/img/asmy.jpg",
-    "url" => "https://www.facebook.com/AsmyMX",
-  ],
-  [
-    "name" => "International Institute for Intelligent Technologies",
-    "img" => "/public/img/iiit.png",
-    "url" => "https://iiitechs.org",
-  ],
+  // [
+  //   "name" => "Association for the Advancement of Intelligent Applications and Technologies with Social Impact",
+  //   "img" => "/public/img/maikron.jpg",
+  //   "url" => "https://maikron.org",
+  // ],
+  // [
+  //   "name" => "Asociación para la Salud Mental de Yucatán",
+  //   "img" => "/public/img/asmy.jpg",
+  //   "url" => "https://www.facebook.com/AsmyMX",
+  // ],
+  // [
+  //   "name" => "International Institute for Intelligent Technologies",
+  //   "img" => "/public/img/iiit.png",
+  //   "url" => "https://iiitechs.org",
+  // ],
 ];
