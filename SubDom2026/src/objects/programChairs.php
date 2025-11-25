a4f8ffb65b88725db65bf93b59db581d6e5336c4
Angel Aldaz <angel.d.aldaz@gmail.com>
2025-11-25 12:20:28 -0600

    feat(content): habilitar listas de program chairs y sponsors en producción
    Se reactivaron los arreglos de programChairs y sponsors, que antes estaban
    comentados, para que ahora se muestren correctamente en el sitio. Esto incluye
    la restauración completa de los datos de cada elemento (nombres, imágenes y URLs).

--- SubDom2026/src/objects/programChairs.php
+++ SubDom2026/src/objects/programChairs.php
@@ -1,25 +1,25 @@
 <?php
 $programChairs = [
 
-  // [
-  //   "first_name" => "Mauricio Gabriel",
-  //   "last_name" => "Orozco-del-Castillo",
-  //   "title" => "Researcher",
-  //   "university" => "Tecnológico Nacional de México/Instituto Tecnológico de Mérida, Mexico.",
-  //   "br" => null
-  // ],
-  // [
-  //   "first_name" => "Juan Antonio",
-  //   "last_name" => "Recio-García",
-  //   "title" => "Researcher",
-  //   "university" => "Universidad Complutense de Madrid.",
-  //   "br" => null
-  // ],
-  // [
-  //   "first_name" => "Pedro",
-  //   "last_name" => "Ortiz-Sánchez",
-  //   "title" => "Researcher",
-  //   "university" => "Tecnológico Nacional de México/Instituto Tecnológico de Mérida, Mexico.",
-  //   "br" => null
-  // ],
+  [
+    "first_name" => "Mauricio Gabriel",
+    "last_name" => "Orozco-del-Castillo",
+    "title" => "Researcher",
+    "university" => "Tecnológico Nacional de México/Instituto Tecnológico de Mérida, Mexico.",
+    "br" => null
+  ],
+  [
+    "first_name" => "Juan Antonio",
+    "last_name" => "Recio-García",
+    "title" => "Researcher",
+    "university" => "Universidad Complutense de Madrid.",
+    "br" => null
+  ],
+  [
+    "first_name" => "Pedro",
+    "last_name" => "Ortiz-Sánchez",
+    "title" => "Researcher",
+    "university" => "Tecnológico Nacional de México/Instituto Tecnológico de Mérida, Mexico.",
+    "br" => null
+  ],
 ];
